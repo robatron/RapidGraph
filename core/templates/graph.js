fa2ea628a96620027503794dd2c7430d4a11811c
Rob McGuire-dale <rob@osuosl.org>
2010-03-02 12:27:36 -0800

    Finished version 2 of poster, started work on js graph.

--- core/templates/graph.js
+++ core/templates/graph.js
@@ -1,6 +1,37 @@
-// graph.js
+// graph.js - A simple graph class
+//   By Rob McGuire-Dale, Feb. 2010
 
 function graph()
 {
+    /////////////////
+    // member data //
+    /////////////////
+    
+    // a node object
+    function node()
+    {
+        this.label = null;
+        this.position = { 
+            'x': null, 
+            'y': null 
+        };
+    }
+    
+    // nodes in the graph
+    var nodes = null;
+    
+    // an edge object
+    function edge()
+    {
+        this.label = null;
+        this.weight = null;
+        this.isDirected = null;
+        this.nodeA = null;
+        this.nodeB = null;
+    }
+    
+    // edges in the graph
+    var edges = null;
+    
     
 }
