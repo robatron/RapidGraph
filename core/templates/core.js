286f349c88d64dd1b9e7979e31231b154165f266
Rob McGuire-Dale <mcguirer@engr.oregonstate.edu>
2010-03-12 00:21:48 -0800

    Code cleanup, and color styling of example of early front-end prototype.

--- core/templates/core.js
+++ core/templates/core.js
@@ -4,15 +4,18 @@ function core()
 {
     this.preload = function()
     {
-        Raphael.fn.connection = function (obj1, obj2, line, bg) 
+        Raphael.fn.connection = function( obj1, obj2, line, bg ) 
         {
-            if (obj1.line && obj1.from && obj1.to) {
+            if( obj1.line && obj1.from && obj1.to ){
+                
                 line = obj1;
                 obj1 = line.from;
                 obj2 = line.to;
             }
+            
             var bb1 = obj1.getBBox();
             var bb2 = obj2.getBBox();
+            
             var p = [{x: bb1.x + bb1.width / 2, y: bb1.y - 1},
                 {x: bb1.x + bb1.width / 2, y: bb1.y + bb1.height + 1},
                 {x: bb1.x - 1, y: bb1.y + bb1.height / 2},
@@ -21,7 +24,9 @@ function core()
                 {x: bb2.x + bb2.width / 2, y: bb2.y + bb2.height + 1},
                 {x: bb2.x - 1, y: bb2.y + bb2.height / 2},
                 {x: bb2.x + bb2.width + 1, y: bb2.y + bb2.height / 2}];
+            
             var d = {}, dis = [];
+            
             for (var i = 0; i < 4; i++) {
                 for (var j = 4; j < 8; j++) {
                     var dx = Math.abs(p[i].x - p[j].x),
@@ -32,11 +37,16 @@ function core()
                     }
                 }
             }
+            
             if (dis.length == 0) {
+            
                 var res = [0, 4];
+            
             } else {
+            
                 var res = d[Math.min.apply(Math, dis)];
             }
+            
             var x1 = p[res[0]].x,
                 y1 = p[res[0]].y,
                 x4 = p[res[1]].x,
@@ -47,12 +57,18 @@ function core()
                 y2 = [y1 - dy, y1 + dy, y1, y1][res[0]].toFixed(3),
                 x3 = [0, 0, 0, 0, x4, x4, x4 - dx, x4 + dx][res[1]].toFixed(3),
                 y3 = [0, 0, 0, 0, y1 + dy, y1 - dy, y4, y4][res[1]].toFixed(3);
+            
             var path = ["M", x1.toFixed(3), y1.toFixed(3), "C", x2, y2, x3, y3, x4.toFixed(3), y4.toFixed(3)].join(",");
+            
             if (line && line.line) {
+            
                 line.bg && line.bg.attr({path: path});
                 line.line.attr({path: path});
+            
             } else {
+            
                 var color = typeof line == "string" ? line : "#000";
+            
                 return {
                     bg: bg && bg.split && this.path(path).attr({stroke: bg.split("|")[0], fill: "none", "stroke-width": bg.split("|")[1] || 3}),
                     line: this.path(path).attr({stroke: color, fill: "none"}),
@@ -68,7 +84,7 @@ function core()
         var el;
         var isDrag = false;
         
-        var dragger = function (e) 
+        var dragger = function(e) 
         {
             this.dx = e.clientX;
             this.dy = e.clientY;
@@ -77,40 +93,63 @@ function core()
             e.preventDefault && e.preventDefault();
         };
         
-        var r = Raphael("workspace", 480 , 480);
-        var connections = [],
-            shapes = [  r.ellipse(190, 100, 30, 20),
-                        r.rect(290, 80, 60, 40, 10),
-                        r.rect(290, 180, 60, 40, 2),
-                        r.ellipse(450, 100, 20, 20)
-                    ];
-        for (var i = 0, ii = shapes.length; i < ii; i++) {
+        // create a new raphael SVG in the "workspace" div
+        var r = Raphael("workspace", 640, 480);
+        
+        var connections = new Array();
+        
+        var shapes = [
+            r.circle(190, 100, 30),
+            r.circle(290, 80, 30),
+            r.circle(290, 180, 30),
+            r.circle(450, 100, 30)
+        ];
+                    
+        for( var i = 0; i < shapes.length; i++ ){
             
-            var color = Raphael.getColor();
-            shapes[i].attr({fill: color, stroke: color, "fill-opacity": 0, "stroke-width": 2});
+            var color = "green" //Raphael.getColor();
+            
+            shapes[i].attr({
+                
+                fill: color, 
+                stroke: color, 
+                "fill-opacity": 0, 
+                "stroke-width": 2
+            });
+            
             shapes[i].node.style.cursor = "move";
+            
             shapes[i].mousedown(dragger);
         }
         
-        connections.push(r.connection(shapes[0], shapes[1], "#fff"));
-        connections.push(r.connection(shapes[1], shapes[2], "#fff", "#fff|5"));
-        connections.push(r.connection(shapes[1], shapes[3], "#000", "#fff"));
+        var edgeFG = "black";
+        var edgeBG = "white";
         
-        document.onmousemove = function (e)
+        connections.push(r.connection(shapes[0], shapes[1], edgeFG, edgeBG));
+        connections.push(r.connection(shapes[1], shapes[2], edgeFG, edgeBG));
+        connections.push(r.connection(shapes[1], shapes[3], edgeFG, edgeBG));
+        
+        document.onmousemove = function( e )
         {
             e = e || window.event;
-            if (isDrag) {
+            
+            if( isDrag ){
+                
                 isDrag.translate(e.clientX - isDrag.dx, e.clientY - isDrag.dy);
-                for (var i = connections.length; i--;) {
+                
+                for( var i = connections.length; i--; ) {
+                
                     r.connection(connections[i]);
                 }
+                
                 r.safari();
+                
                 isDrag.dx = e.clientX;
                 isDrag.dy = e.clientY;
             }
         };
         
-        document.onmouseup = function ()
+        document.onmouseup = function()
         {
             isDrag && isDrag.animate({"fill-opacity": 0}, 500);
             isDrag = false;
