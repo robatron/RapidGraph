a8cdfa411a277001f9f4f78e0ee78fea28873744
Rob McGuire-Dale <mcguirer@engr.oregonstate.edu>
2010-03-24 01:23:10 -0700

    graph class created seperate from the UI.

--- core/templates/rapidgraph.graph.js
+++ core/templates/rapidgraph.graph.js
@@ -2,50 +2,165 @@
 
 function rapidgraph_graph( surface )
 {    
-    //////////
-    // DATA //
-    //////////
+    ////////////////
+    // GRAPH DATA //
+    ////////////////
     
-    this.surface = surface;         // Raphael SVG drawing surface for this graph
-    var grabbedNode = null;     // the currently grabbed node
-
+    // public data
+    this.surface = surface;     // Raphael SVG drawing surface for this graph
+    
     var nodes = new Array();    // an array of the nodes
     var edges = new Array();    // an array of the edges
     
-    // a node object
-    function node()
-    {
-        this.selected = false;
-        this.label = null;
-        this.position = { 
-            'x': null, 
-            'y': null 
-        };
-    }
+    // private data
+    var grabbedNode = null;     // the currently grabbed node
     
-    // an edge object
-    function edge()
-    {        
-        this.nodeA = null;
-        this.nodeB = null;
-        
-        this.label = null;
-        this.selected = false;
-        
-        this.weight = null;
-        this.isDirected = null;
-    }
+    // grab the surface offset values. (For some reason the .offset() values
+    // change after each call)
+    var offset = {
+        x: $(surface.canvas).offset().left,
+        y: $(surface.canvas).offset().top
+    };
+    
+    // run init as soon as a graph object is instantiated
+    init();
     
     //////////////////////
     // PUBLIC FUNCTIONS //
     //////////////////////
     
-    // initialize a new graph
-    function init()
+    this.clear = function()
     {
+        console.group("graph.init()");
+
+        surface.clear();
+        
+        console.log("surface cleared");
+        console.groupEnd();
     }
     
-    //////////////////////////////
-    // PRIVATE HELPER FUNCTIONS //
-    //////////////////////////////
+    //////////
+    // INIT //
+    //////////
+    
+    function init()
+    // initialize the graph
+    {
+        console.group("graph.init()");
+        
+        surface.canvas.onmousemove = function(e)
+        // if there's currently a grabbed node, move it to the mouse's position
+        {            
+            if( grabbedNode ){
+                var n = grabbedNode;
+                n.translate( e.clientX - n.dx, e.clientY - n.dy );
+                n.dx = e.clientX;
+                n.dy = e.clientY;
+            }
+        };
+        
+        surface.canvas.onmousedown = function( e )
+        // if no node is under the pointer, make a new node
+        {
+            console.group("document.onmousedown");
+            
+            if( !grabbedNode ){
+                
+                var x = e.clientX - offset.x;
+                var y = e.clientY - offset.y;
+                
+                console.log("creating new node at %d, %d", x, y);
+                
+                grabbedNode = newNode( x, y );
+            }
+            
+            console.groupEnd();
+        };
+        
+        surface.canvas.onmouseup = function()
+        // if the mouse button is lifted, clear any grabbed nodes
+        { 
+            console.group("document.onmouseup");
+            
+            grabbedNode = null;
+            
+            console.log("Mouse lifted. grabbedNode cleared");
+            console.groupEnd();
+        };
+        
+        console.groupEnd();
+    }
+    
+    ////////////////////
+    // NODE FUNCTIONS //
+    ////////////////////
+    
+    // a node object
+    function node( settings )
+    {        
+        console.group("graph.node");
+        console.log("creating new node");
+        
+        // default settings
+        var defaultSettings = {
+            radius:     20,
+            x:          surface.width/2-this.radius,
+            y:          surface.height/2-this.radius,
+            selected:   false,
+            label:      null,
+            fill:       "black",
+            stroke:     "white"
+        }
+        
+        // extend the default settings with the passed-in settings
+        $.extend( defaultSettings, settings );
+        
+        // make settings public
+        this.settings = defaultSettings;
+        
+        // the raphael object
+        this.object = surface.circle( 
+            this.settings.x, 
+            this.settings.y, 
+            this.settings.radius 
+        ).attr({
+            fill: this.settings.fill,
+            stroke: this.settings.stroke
+        });
+        
+        console.groupEnd();
+        
+        return this.object;
+    }
+    
+    function newNode( x, y )
+    // create a new node at x,y and push it onto the nodes stack
+    {
+        console.group("graph.newNode()");
+        console.log("pushing new node onto stack");
+        
+        // make a new node and push it onto the stack
+        nodes.push( new node({ x:x, y:y }) );
+        
+        // change the mouseover cursor to the "move" symbol
+        nodes[nodes.length-1].node.style.cursor = "move";
+        
+        // set the mousedown
+        nodes[nodes.length-1].mousedown( function(e)
+        {                
+            // set the grabbed node to this node
+            grabbedNode = this;
+            
+            // set this node's position to the mouse's position
+            grabbedNode.dx = e.clientX;
+            grabbedNode.dy = e.clientY;
+            
+            // prevent the default event action
+            e.preventDefault();
+        });
+        
+        console.groupEnd();
+        
+        return nodes[nodes.length-1];
+    }
 }
--- core/templates/rapidgraph.ui.js
+++ core/templates/rapidgraph.ui.js
@@ -9,6 +9,8 @@ function rapidgraph_ui()
     var grabbedNode = null; // currently grabbed node. Null if none grabbed.
     var nodes = new Array(); // the nodes
     
+    var graph = null;
+    
     this.start = function()
     {
         console.group("ui.start()");
@@ -37,66 +39,8 @@ function rapidgraph_ui()
         var width = 1000;
         var height = 300;
         surface = Raphael( "ui", width, height );
-        console.log("New Raphael object created of size %dx%d", width, height); 
-        
-        /* TODO: Figure out dynamic scaling
-        $(window).resize(function(){
-            $("#ui").scale();
-        });
-        */
-        
-        // set up notepad
-        $("#ui").mousemove( function( e )
-        {
-            // if there's currently a grabbed node, move the node to the mouse's
-            // position
-            if( grabbedNode ){
-                var n = grabbedNode;
-                n.translate( e.clientX - n.dx, e.clientY - n.dy );
-                
-                n.dx = e.clientX;
-                n.dy = e.clientY;
-            }
-        });
-        
-        // if the mouse button is lifted, clear any grabbed nodes
-        $("#ui").mouseup( function(){ grabbedNode = null });
-        
-        $("#ui").mousedown( function(e){
-            if( !grabbedNode ){
-                var x = e.clientX - this.offsetLeft;
-                var y = e.clientY - this.offsetTop;
-                grabbedNode = newNode( x, y );
-            }
-        });
+        graph = new rapidgraph_graph( surface );
         
         console.groupEnd();
-    }
-    
-    function newNode( x, y )
-    {
-        var radius = 10;
-        
-        nodes.push( surface.circle( x, y, radius ).attr({
-            fill: "lightgreen",
-            stroke: "green"
-        }));
-        
-        nodes[nodes.length-1].node.style.cursor = "move";
-        
-        nodes[nodes.length-1].mousedown( function(e)
-        {                
-            // set the grabbed node to this node
-            grabbedNode = this;
-            
-            // set this node's position to the mouse's position
-            grabbedNode.dx = e.clientX;
-            grabbedNode.dy = e.clientY;
-            
-            // prevent the default event action
-            e.preventDefault();
-        });
-        
-        return nodes[nodes.length-1];
     }
 }
