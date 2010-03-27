d3b572af4442d37e7fc66e0d03a87a06438c936a
Rob McGuire-Dale <mcguirer@engr.oregonstate.edu>
2010-03-27 01:21:25 -0700

    edge update working with more complex path.

--- core/templates/rapidgraph_graph.js
+++ core/templates/rapidgraph_graph.js
@@ -503,7 +503,7 @@ function rapidgraph_graph( surface )
             node1:  null,
             node2:  null,
             fill:   "white",
-            stroke:     "white"
+            stroke: "white"
         }
         
         // extend the default attributes with the passed-in attributes
@@ -514,19 +514,80 @@ function rapidgraph_graph( surface )
         
         // DO SOME SWEET STUFFS (comment later) --------------------------------
         this.update = function()
-        {
-            if( this.object ) this.object.remove();
+        {            
+            var bb1 = this.attr.node1.object.getBBox();
+            var bb2 = this.attr.node2.object.getBBox();
             
-            var n1x = this.attr.node1.object.getBBox().x;
-            var n1y = this.attr.node1.object.getBBox().y;
-            var n2x = this.attr.node2.object.getBBox().x;
-            var n2y = this.attr.node2.object.getBBox().y;
-            var pathParams = "M"+n1x+" "+n1y+"L"+n2x+" "+n2y;
+            var p = [
+                {x: bb1.x + bb1.width / 2,  y: bb1.y - 1},
+                {x: bb1.x + bb1.width / 2,  y: bb1.y + bb1.height + 1},
+                {x: bb1.x - 1,              y: bb1.y + bb1.height / 2},
+                {x: bb1.x + bb1.width + 1,  y: bb1.y + bb1.height / 2},
+
+                {x: bb2.x + bb2.width / 2,  y: bb2.y - 1},
+                {x: bb2.x + bb2.width / 2,  y: bb2.y + bb2.height + 1},
+                {x: bb2.x - 1,              y: bb2.y + bb2.height / 2},
+                {x: bb2.x + bb2.width + 1,  y: bb2.y + bb2.height / 2}
+            ];
             
-            this.object = surface.path( pathParams ).attr({
-                stroke: "white",
-                'stroke-width': 3
-            });
+            var d = {};
+            var dis = [];
+            
+            for( var i = 0; i < 4; i++ )
+                for( var j = 4; j < 8; j++ ){
+                    
+                    var dx = Math.abs(p[i].x - p[j].x);
+                    var dy = Math.abs(p[i].y - p[j].y);
+                    
+                    if( (i == j - 4) 
+                        || (((i != 3 && j != 6) || p[i].x < p[j].x) 
+                        && ((i != 2 && j != 7) || p[i].x > p[j].x) 
+                        && ((i != 0 && j != 5) || p[i].y > p[j].y) 
+                        && ((i != 1 && j != 4) || p[i].y < p[j].y))){
+                            
+                        dis.push(dx + dy);
+                        d[dis[dis.length - 1]] = [i, j];
+                    }
+                }
+            
+            if (dis.length == 0)
+                var res = [0, 4];
+            else
+                var res = d[Math.min.apply(Math, dis)];
+            
+            var x1 = p[res[0]].x;
+            var y1 = p[res[0]].y;
+            var x4 = p[res[1]].x;
+            var y4 = p[res[1]].y;
+            var dx = Math.max(Math.abs(x1 - x4) / 2, 10);
+            var dy = Math.max(Math.abs(y1 - y4) / 2, 10);
+            var x2 = [x1, x1, x1 - dx, x1 + dx][res[0]].toFixed(3);
+            var y2 = [y1 - dy, y1 + dy, y1, y1][res[0]].toFixed(3);
+            var x3 = [0, 0, 0, 0, x4, x4, x4 - dx, x4 + dx][res[1]].toFixed(3);
+            var y3 = [0, 0, 0, 0, y1 + dy, y1 - dy, y4, y4][res[1]].toFixed(3);
+            
+            var path = [
+                "M", x1.toFixed(3), 
+                y1.toFixed(3), 
+                "C", 
+                x2, 
+                y2, 
+                x3, 
+                y3, 
+                x4.toFixed(3), 
+                y4.toFixed(3)
+            ].join(",");
+            
+            // if the object is already defined, just update the path
+            if( this.object ) 
+                this.object.attr({ path: path });
+                
+            // otherwise, create a new path
+            else
+                this.object = surface.path( path ).attr({
+                    stroke: "white",
+                    'stroke-width': 3
+                });
         }
         
         this.update();
