d3ea3a48ffd8be867670ccb087506d3f2e884c19
Rob McGuire-Dale <mcguirer@engr.oregonstate.edu>
2010-04-16 00:58:14 -0700

    Changing views to pass in the plugin manager's return value.

--- core/views.py
+++ core/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render_to_response
+from plugin_manager import *
 
 # The main application view
 def index( request ):
-    return render_to_response('RapidGraph.html')
+    return render_to_response('RapidGraph.html', {
+        'plugins':get_plugins()
+    })
