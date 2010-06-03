aac05b6d6dd2e48f3cd3953ab8f754daa3516e2f
Michael Anderson <andermic@morphy.(none)>
2010-06-02 20:52:06 -0700

    Still working on Eucler

--- plugins/eulerCircuit/plugin.js
+++ plugins/eulerCircuit/plugin.js
@@ -12,7 +12,7 @@ this.init = function()
 {
     safelog("::plugin:: is initializing!");
     
-    $("#::plugin::_euler").click( function(){
+    $("#::plugin::_euler").click( function() {
         euler();
     });
 }
@@ -27,7 +27,7 @@ this.stop = function()
     safelog("::plugin:: is stopping!");
 }
 
-// Find an Euler Circuit if there is one, by checking to see if every
+// Find an Euler circuit if there is one, by checking to see if every
 // vertex in the current graph has an even degree. If there is one,
 // create a directed graph that represents a possible circuit.
 function euler() {
@@ -35,27 +35,50 @@ function euler() {
     
     nodes = graph.nodes.get.all();
     edges = graph.edges.get.all();
-    for( i = 0; i < nodes.length; ++i ) {
-        // Figure out the degree of each node by counting all of its
-        // attached edges.
-        degree = 0;
-        for( j = 0; j < edges.length; ++j ) {
-            if(edges[j].attachedTo(nodes[i])) {
-                ++degree;
-            }
-        }
-        
+    eList = new Array();
+    for( var i = 0; i < edges.length; ++i) {
+        eList.push(i);
+    }
+    
+    for( var i = 0; i < nodes.length; ++i ) {
         // If a node with odd degree is found, alert the user and return
-        if( degree % 2 == 1 ) {
+        if( neighbors(i).length % 2 == 1 ) {
             alert("Graph does not contain an Euler circuit");
             return;
         }
+        console.log(neighbors(i).length);
     }
     
-    // Since the previous loop did not return, an Euler circuit was
-    // found. Show one through a series of directed edges.
-    n = nodes[0];
-    while( edges.length > 0 ) {
-        
+    // Since the previous loop did not return, the graph has Euler
+    //  circuits
+    n = 0;
+    while( eList.length > 0 ) {
+        curNeighbors = neighbors(n);
+        highNeighbor = curNeighbors[0];
+        for( var i = 1; i < curNeighbors.length; ++i ) {
+            if( neighbors(curNeighbors[i]).length > neighbors(highNeighbor).length )
+                highNeighbor = curNeighbors[i];
+        }
+        for( var i = 0; i < edges.length; ++i ) {
+            if( edges[i].attachedTo(nodes[n]) && edges[i].attachedTo(nodes[highNeighbor]) ) {
+                api.graph.edges.createNew({
+                    node1: nodes[n],
+                    node2: nodes[highNeighbor],
+                    directed: true,
+                    selected: true,
+                    text: null
+                });
+            }
+        }
     }
+}
+
+function neighbors(nIndex) {
+    var neighbors = new Array();
+    for( var i = 0; i < eList.length; ++i ) {
+        if( edges[eList[i]].attachedTo(nodes[nIndex]) ) {
+            neighbors.push(i);
+        }
+    }
+    return neighbors;
 }
