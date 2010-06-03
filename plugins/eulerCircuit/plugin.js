09458c776d598dc10f503b23859f7810dcb90e99
Michael Anderson <andermic@morphy.(none)>
2010-06-02 23:02:02 -0700

    Finished with Euler plugin. Commented out Undo/Redo button html.

--- plugins/eulerCircuit/plugin.js
+++ plugins/eulerCircuit/plugin.js
@@ -46,38 +46,61 @@ function euler() {
             alert("Graph does not contain an Euler circuit");
             return;
         }
-        console.log(neighbors(i).length);
     }
     
-    // Since the previous loop did not return, the graph has Euler
-    //  circuits
     n = 0;
-    while( eList.length > 0 ) {
+    // Since the previous loop did not return, the graph has Euler
+    //  circuits. Starting with the first node, traverse edges one by
+    //  one until they are all exhausted.
+    for( var passes = 1; eList.length > 0; ++passes ) {
         curNeighbors = neighbors(n);
         highNeighbor = curNeighbors[0];
+        
+        // Find the neighbor of the current node that itself has the
+        //  highest number of neighbors. That will be the destination
+        //  node for this pass.
         for( var i = 1; i < curNeighbors.length; ++i ) {
             if( neighbors(curNeighbors[i]).length > neighbors(highNeighbor).length )
                 highNeighbor = curNeighbors[i];
         }
-        for( var i = 0; i < edges.length; ++i ) {
-            if( edges[i].attachedTo(nodes[n]) && edges[i].attachedTo(nodes[highNeighbor]) ) {
+        
+        // Make a directed edge from the current node to its neighbor
+        //  with the highest number of edges.
+        for( var i = 0; i < eList.length; ++i ) {
+            if( edges[eList[i]].attachedTo(nodes[n]) && edges[eList[i]].attachedTo(nodes[highNeighbor]) ) {
                 api.graph.edges.createNew({
                     node1: nodes[n],
                     node2: nodes[highNeighbor],
                     directed: true,
                     selected: true,
-                    text: null
+                    text: passes
                 });
+                eList.splice(i, 1);
+                break;
             }
         }
+        
+        // Make the destination node of the edge eliminated in this
+        //  traversal the new source for the next pass through.
+        n = highNeighbor;
     }
+    // Eliminate all traversed edges from the UI
+    for( var i = 0; i < edges.length/2; ++i )
+        edges[i].remove();
+    // Select all remaining edges in the UI
+    for( var i = 0; i < edges.length; ++i )
+        edges[i].select();
 }
 
+// Takes the index of the given node, returns the index of each of its
+//  neighbors.
 function neighbors(nIndex) {
     var neighbors = new Array();
-    for( var i = 0; i < eList.length; ++i ) {
-        if( edges[eList[i]].attachedTo(nodes[nIndex]) ) {
-            neighbors.push(i);
+    for( var i = 0; i < nodes.length; ++i ) {
+        for( var j = 0; j < eList.length; ++j ) {
+            if( i != nIndex && edges[eList[j]].attachedTo(nodes[i]) && edges[eList[j]].attachedTo(nodes[nIndex]) ) {
+                neighbors.push(i);
+            }
         }
     }
     return neighbors;
